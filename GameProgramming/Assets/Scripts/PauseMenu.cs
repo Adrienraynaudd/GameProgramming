21faa9ec40d4fad3394d8e967fc1eb90314abfb5
Lupit0t <paul.piauger@ynov.com>
2023-03-03 16:58:49 +0100

    menu pause

---
+++ GameProgramming/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+        
+    }
+
+    public void Resume()
+    {
+        // PlayerMovement.instance.enabled = true;
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        // PlayerMovement.instance.enabled = false;
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    // public void LoadMenu()
+    // {
+    //     DontDestroyOnLoadScene.instance.RemoveFromDontDestroyOnLoad();
+    //     SceneManager.LoadScene("MainMenu");
+    // }
+}
