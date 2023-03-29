aa6ba91064c99ae17667ac0d43b26da50865e9a8
ARAYNAUD <adrien.raynaud@ynov.com>
2023-03-29 11:20:26 +0200

    continue bouton

--- GameProgramming/Assets/Scripts/MainMenu.cs
+++ GameProgramming/Assets/Scripts/MainMenu.cs
@@ -27,4 +27,8 @@ public class MainMenu : MonoBehaviour
     {
         SceneManager.LoadScene("credits");
     }
+    public void ContinueButton()
+    {
+        SceneManager.LoadScene("SelectLvl");
+    }
 }
